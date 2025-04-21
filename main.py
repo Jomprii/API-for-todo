562f0624fad6fdfc6d1b2a0d4bec7b12922bc446
Jomprii <jyamson30@gmail.com>
2025-04-21 20:16:21 +0800

    API

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
-from . import models, schemas, database
+import models
+import schemas
+import database
 
 models.Base.metadata.create_all(bind=database.engine)
 

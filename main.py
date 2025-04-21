895caa5c776393be423c1cb679984d13f2964a80
Jomprii <jyamson30@gmail.com>
2025-04-21 20:21:50 +0800

    New Commit

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/tasks", response_model=list[schemas.Task])
+@app.get("/Junprii", response_model=list[schemas.Task])
 def read_tasks(db: Session = Depends(get_db)):
     return db.query(models.Task).all()
 
-@app.post("/tasks", response_model=schemas.Task)
+@app.post("/Junprii", response_model=schemas.Task)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     db_task = models.Task(**task.dict())
     db.add(db_task)
@@ -37,7 +37,7 @@ def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     db.refresh(db_task)
     return db_task
 
-@app.put("/tasks/{task_id}", response_model=schemas.Task)
+@app.put("/Junprii/{task_id}", response_model=schemas.Task)
 def update_task(task_id: int, updated_task: schemas.TaskUpdate, db: Session = Depends(get_db)):
     task = db.query(models.Task).filter(models.Task.id == task_id).first()
     if not task:
@@ -47,7 +47,7 @@ def update_task(task_id: int, updated_task: schemas.TaskUpdate, db: Session = De
     db.commit()
     return task
 
-@app.delete("/tasks/{task_id}")
+@app.delete("/Junprii/{task_id}")
 def delete_task(task_id: int, db: Session = Depends(get_db)):
     task = db.query(models.Task).filter(models.Task.id == task_id).first()
     if not task:
